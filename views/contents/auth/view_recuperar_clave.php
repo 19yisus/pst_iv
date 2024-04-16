066a636e0e1e121ed47e9eed9d74c522b25f38c1
Gabriel Valera <gabrielj2814@hotmail.com>
2024-04-15 22:22:32 -0400

    listo viva chavez

--- views/contents/auth/view_recuperar_clave.php
+++ views/contents/auth/view_recuperar_clave.php
@@ -176,14 +176,21 @@ if (isset($_POST['op'])) {
                               Cedula del Usuario<span class="ml-4 text-meta-1 ">*</span>
                             </label>
                             <input type="text" placeholder="" readonly value="<?php echo $cedula_usuario; ?>" name="cedula_usuario" class=" w-full rounded border-[1.5px] border-stroke bg-transparent py-3 px-5 font-medium outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:focus:border-primary" />
+                            <span class="absolute" style="right: 20px;bottom: 14px;" onclick="mostrarClave('pass_user',this)">
+                              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-eye"><path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"></path><circle cx="12" cy="12" r="3"></circle></svg>
+                            </span>
                           </div>
-                          <div class=" m-4 w-full xl:w-2/6">
+                          <div class=" m-4 w-full xl:w-2/6 relative">
                             <label class="mb-2.5 block text-black dark:text-white">
                               Contraseña<span class="ml-4 text-meta-1 ">*</span>
                             </label>
-                            <input type="password" placeholder="" required pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" name="clave_usuario" class="w-full rounded border-[1.5px] border-stroke bg-transparent py-3 px-5 font-medium outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:focus:border-primary" />
+                            <input type="password" placeholder=""  onkeyup="validarPassword()" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" name="clave_usuario" id="clave_usuario" class="w-full rounded border-[1.5px] border-stroke bg-transparent py-3 px-5 font-medium outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:focus:border-primary" />
+                            <span class="absolute" style="right: 20px;bottom: 14px;" onclick="mostrarClave('clave_usuario',this)">
+                              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-eye"><path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"></path><circle cx="12" cy="12" r="3"></circle></svg>
+                            </span>
                           </div>
                         </div>
+                        <div id="messages_clave"></div>
                         <div id="abrir" class="flex flex-row justify-center items-center mb-6 ">
                           <button type="submit" class=" flex w-72 flex-col justify-center items-center rounded bg-primary p-3 font-medium text-gray cursor-pointer">
                             <label class="" for="btn">Enviar</label>
@@ -194,6 +201,7 @@ if (isset($_POST['op'])) {
                   </div>
                 </div>
               </div>
+              
             </div>
           <?php
           }
@@ -204,6 +212,56 @@ if (isset($_POST['op'])) {
   </div>
   <!-- ===== Page Wrapper End ===== -->
   <?php $this->GetComplement('scripts'); ?>
+  <script>
+    function mostrarClave(idInputPass,contendorIcon){
+			let input=document.getElementById(idInputPass)
+			if(input.type=="password"){
+				input.type="text"
+				contendorIcon.innerHTML=`
+					<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-eye-off"><path d="M17.94 17.94A10.07 10.07 0 0 1 12 20c-7 0-11-8-11-8a18.45 18.45 0 0 1 5.06-5.94M9.9 4.24A9.12 9.12 0 0 1 12 4c7 0 11 8 11 8a18.5 18.5 0 0 1-2.16 3.19m-6.72-1.07a3 3 0 1 1-4.24-4.24"></path><line x1="1" y1="1" x2="23" y2="23"></line></svg>
+				`
+			}
+			else{
+				input.type="password"
+				contendorIcon.innerHTML=`
+					<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-eye"><path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"></path><circle cx="12" cy="12" r="3"></circle></svg>
+				`
+			}
+		}
+
+    function validarPassword() {
+			const password = document.getElementById('clave_usuario').value;
+			const messages = document.getElementById('messages_clave');
+			messages.innerHTML = '';
+
+			// Validaciones
+			let valido = true;
+
+			if (password.length < 7) {
+				messages.innerHTML += 'La contraseña debe tener al menos 7 caracteres.<br>';
+				valido = false;
+			}
+
+			if (!/[A-Z]/.test(password)) {
+				messages.innerHTML += 'La contraseña debe contener al menos una mayúscula.<br>';
+				valido = false;
+			}
+
+			if (!/\d/.test(password)) {
+				messages.innerHTML += 'La contraseña debe contener al menos un número.<br>';
+				valido = false;
+			}
+
+			if (!/[^a-zA-Z0-9]/.test(password)) {
+				messages.innerHTML += 'La contraseña debe contener al menos un caracter especial ejemplo puede ser (@, #, &, $ etc...).<br>';
+				valido = false;
+			}
+
+			if (valido) {
+				messages.innerHTML = 'La contraseña cumple con todas las condiciones.';
+			}
+		}
+  </script>
 </body>
 
 </html>
--- views/contents/profile/view_index.php
+++ views/contents/profile/view_index.php
@@ -302,10 +302,14 @@
 												<label class="mb-2.5 block font-medium text-black dark:text-white">Contraseña <span
 														class="text-meta-1">*</span></label>
 												<div class="relative">
-													<input type="password" autocomplete="off" maxlength="60" placeholder="Ingrese su clave"
-														name="clave_usuario" value=""
+													<input type="password" onkeyup="validarPassword()" autocomplete="off" maxlength="60" placeholder="Ingrese su clave"
+														name="clave_usuario" id="clave_usuario" value=""
 														class="w-full rounded-lg border border-stroke bg-transparent py-4 pl-6 pr-10 outline-none focus:border-primary focus-visible:shadow-none dark:border-form-strokedark dark:bg-form-input dark:focus:border-primary" />
+														<span class="absolute" style="right: 20px;bottom: 16px;" onclick="mostrarClave('clave_usuario',this)">
+															<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-eye"><path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"></path><circle cx="12" cy="12" r="3"></circle></svg>
+														</span>
 												</div>
+												<div id="messages_clave"></div>
 											</div>
 											
 										</div>
@@ -328,6 +332,56 @@
 	</div>
 	<!-- ===== Page Wrapper End ===== -->
 	<?php $this->GetComplement('scripts'); ?>
+	<script>
+    function mostrarClave(idInputPass,contendorIcon){
+			let input=document.getElementById(idInputPass)
+			if(input.type=="password"){
+				input.type="text"
+				contendorIcon.innerHTML=`
+					<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-eye-off"><path d="M17.94 17.94A10.07 10.07 0 0 1 12 20c-7 0-11-8-11-8a18.45 18.45 0 0 1 5.06-5.94M9.9 4.24A9.12 9.12 0 0 1 12 4c7 0 11 8 11 8a18.5 18.5 0 0 1-2.16 3.19m-6.72-1.07a3 3 0 1 1-4.24-4.24"></path><line x1="1" y1="1" x2="23" y2="23"></line></svg>
+				`
+			}
+			else{
+				input.type="password"
+				contendorIcon.innerHTML=`
+					<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-eye"><path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"></path><circle cx="12" cy="12" r="3"></circle></svg>
+				`
+			}
+		}
+
+    function validarPassword() {
+			const password = document.getElementById('clave_usuario').value;
+			const messages = document.getElementById('messages_clave');
+			messages.innerHTML = '';
+
+			// Validaciones
+			let valido = true;
+
+			if (password.length < 7) {
+				messages.innerHTML += 'La contraseña debe tener al menos 7 caracteres.<br>';
+				valido = false;
+			}
+
+			if (!/[A-Z]/.test(password)) {
+				messages.innerHTML += 'La contraseña debe contener al menos una mayúscula.<br>';
+				valido = false;
+			}
+
+			if (!/\d/.test(password)) {
+				messages.innerHTML += 'La contraseña debe contener al menos un número.<br>';
+				valido = false;
+			}
+
+			if (!/[^a-zA-Z0-9]/.test(password)) {
+				messages.innerHTML += 'La contraseña debe contener al menos un caracter especial ejemplo puede ser (@, #, &, $ etc...).<br>';
+				valido = false;
+			}
+
+			if (valido) {
+				messages.innerHTML = 'La contraseña cumple con todas las condiciones.';
+			}
+		}
+  </script>
 </body>
 
 </html>
